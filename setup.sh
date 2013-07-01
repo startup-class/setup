5e142e7736af08f0266518eba4e24ab76311ab48
Balaji S. Srinivasan <balajis@stanford.edu>
2013-07-01 16:28:30 -0700

    Added setup script. Also clones dotfiles repo.

---
+++ setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
+# for headless setup. 
+
+# Install nvm: node-version manager
+# https://github.com/creationix/nvm
+sudo apt-get install -y git-core
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+# Load nvm and install latest production node
+source $HOME/.nvm/nvm.sh
+nvm install v0.10.12
+nvm use v0.10.12
+
+# Install jshint to allow checking of JS code within emacs
+# http://jshint.com/
+npm install -g jshint
+
+# Install rlwrap to provide libreadline features with node
+# See: http://nodejs.org/api/repl.html#repl_repl
+sudo apt-get install -y rlwrap
+
+# Install emacs24
+# https://launchpad.net/~cassou/+archive/emacs
+sudo apt-add-repository -y ppa:cassou/emacs
+sudo apt-get update
+sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
+
+# git pull and install dotfiles as well
+cd $HOME
+if [ -d ./dotfiles/ ]; then
+    mv dotfiles dotfiles.old
+fi
+if [ -d .emacs.d/ ]; then
+    mv .emacs.d .emacs.d~
+fi
+git clone https://github.com/startup-class/dotfiles.git
+ln -sb dotfiles/.screenrc .
+ln -sb dotfiles/.bash_profile .
+ln -sb dotfiles/.bashrc .
+ln -sb dotfiles/.bashrc_custom .
+ln -sf dotfiles/.emacs.d .
+
