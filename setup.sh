8177a00bee77864d4c4aa2c41552550f90190161
Balaji S. Srinivasan <balajis@stanford.edu>
2013-07-17 18:45:18 -0700

    Added installation of curl. Fixes #4

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
 sudo apt-get install -y git-core
+sudo apt-get install -y curl
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
 # Load nvm and install latest production node
